0fb2dee377f1c028263f103400e989b040b6f14d
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-11-28 15:20:38 -0600

    completed step 1 of HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+(1) Write a program that outputs the following.
+
+Hello world!
+
+(2) Update to output the following.
+
+Hello world!
+How are you? 
+
+(3) Finally, update to output the following. End with a newline.
+
+Hello world!
+How are you?
+   (I'm fine).
+*/
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.print("Hello world!");
+
+      return;
+   }
+}
