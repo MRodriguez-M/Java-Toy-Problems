a8f3008c2c6da3ac0c1fe516b9758bdaf804593e
unknown <melissa.rodriguez.murguia@gmail.com>
2020-03-13 04:42:43 -0600

    completed step 2 and finished GradeCalculation

--- GradeCalculation.java
+++ GradeCalculation.java
@@ -25,24 +25,35 @@ public class GradeCalculation {
         double homeworkWeighted = 0;
         double midtermWeighted = 0;
         double finalWeighted = 0;
+        double quizzes = 0.0;
+        double quizWeighted = 0.0;
+        double totalGrade = 0.0;
 
-        // Prompt user for scores in homework, the midterm, and the final
+        // Prompt user for scores in homework, quizzes, the midterm, and the final
         System.out.print("Enter homework score: ");
         homework = scnr.nextDouble();
+        System.out.print("Enter quiz score: ");
+        quizzes = scnr.nextDouble();
         System.out.print("Enter midterm exam score: ");
         midterm = scnr.nextDouble();
         System.out.print("Enter final exam score: ");
         finalExam = scnr.nextDouble();
 
         // Calculate the weighted score for each section
-        homeworkWeighted = (homework / 80 * 0.2) * 100;
+        homeworkWeighted = (homework / 80 * 0.1) * 100;
         midtermWeighted = (midterm / 40 * 0.3) * 100;
-        finalWeighted = (finalExam / 70 * 0.5) * 100;
+        finalWeighted = (finalExam / 70 * 0.45) * 100;
+        quizWeighted = (quizzes / 20 * 0.15) * 100;
 
         // Output the weighted score for each section
         System.out.println("\nGrade Total Breakdown:");
         System.out.printf("Homework: %.2f%%", homeworkWeighted);
+        System.out.printf("\nQuizzes: %.2f%%", quizWeighted);
         System.out.printf("\nMidterm exam: %.2f%%", midtermWeighted);
         System.out.printf("\nFinal exam: %.2f%%", finalWeighted);
+
+        // Calculate and output final grade based on weighted percentages
+        totalGrade = homeworkWeighted + quizWeighted + midtermWeighted + finalWeighted;
+        System.out.printf("\n\nFinal grade: %.2f%%", totalGrade);
     }
 }
